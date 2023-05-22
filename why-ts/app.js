9255d78fd0470bdc32624ddb17d7297f966d0159
baeggoose <bsj154@gmail.com>
2023-05-22 23:59:54 +0900

    타입스크립트 쓰는이유ex)address street

--- why-ts/app.js
+++ why-ts/app.js
@@ -19,7 +19,7 @@ function startApp() {
       console.log(user);
       username.innerText = user[0].name;
       email.innerText = user[0].email;
-      address.innerText = user[0].addr;
+      address.innerText = user[0].address.street;
     })
     .catch(function (error) {
       console.log(error);
