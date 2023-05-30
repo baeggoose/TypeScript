65fa10571f2a307e9689f145f99a103f535c2206
baeggoose <bsj154@gmail.com>
2023-05-30 23:59:34 +0900

    기본타입_문자열,숫자,배열

---
+++ class-note/1_type_basic.ts
@@ -0,0 +1,10 @@
+// js문자열 선언
+// var str = "hello";
+
+// ts문자열 선언
+let str: string = "hello";
+let num: number = 10;
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["baeggoose", "penguin", 10];
+// 배열 리터럴
+let items: number[] = [1, 2, 3];
