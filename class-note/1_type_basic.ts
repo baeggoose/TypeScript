b7ac6223eb41f5898a69c50d36f8739b373cbdcd
baeggoose <bsj154@gmail.com>
2023-06-03 11:19:46 +0900

    기본타입_튜플[모든 인덱스의 타입을 지정]

--- class-note/1_type_basic.ts
+++ class-note/1_type_basic.ts
@@ -1,10 +1,15 @@
 // js문자열 선언
 // var str = "hello";
 
-// ts문자열 선언
+// TS 문자열 선언
 let str: string = "hello";
+// TS 숫자 선언
 let num: number = 10;
+// TS 배열 선언
 let arr: Array<number> = [1, 2, 3];
 let heroes: Array<string> = ["baeggoose", "penguin", 10];
 // 배열 리터럴
 let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ["gangnam", 100];
