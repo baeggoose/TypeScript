2cd8b38d13d04400bd2958f6ef0d4760cf98bc0c
baeggoose <bsj154@gmail.com>
2023-08-21 13:54:50 +0900

    Union Type

---
+++ class-note/5_operator.ts
@@ -0,0 +1,11 @@
+// function logMessage(value: any) {
+//   console.log(value);
+// }
+// logMessage("hi");
+// logMessage(100);
+
+function logMessage(value: string | number) {
+  console.log(value);
+}
+logMessage("hi");
+logMessage(100);
