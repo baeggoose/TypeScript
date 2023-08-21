167e75d773a8c4b143e26bb7f0a3076f65bc9c76
baeggoose <bsj154@gmail.com>
2023-08-21 15:44:49 +0900

    Union Type, Intersection Type

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -4,8 +4,37 @@
 // logMessage("hi");
 // logMessage(100);
 
+var ggoose: string | number | boolean;
 function logMessage(value: string | number) {
-  console.log(value);
+  if (typeof value === "number") {
+    value.toLocaleString();
+  }
+  if (typeof value === "string") {
+    value.toString();
+  }
+  throw new TypeError("value must be string or number");
 }
 logMessage("hi");
 logMessage(100);
+
+interface Dev {
+  name: string;
+  skill: string;
+}
+interface Human {
+  name: string;
+  age: number;
+}
+
+// function askSomenone(someone: Dev | Human) {
+//   someone.name;
+// }
+// askSomenone({name:'백구',skill:'웹개발'})
+// askSomenone({name:'구스',age:100})
+
+function askSomenone(someone: Dev & Human) {
+  someone.name;
+  someone.skill;
+  someone.age;
+}
+askSomenone({ name: "백구", skill: "웹개발", age: 109 });
