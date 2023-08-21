d3280c2ce597da6793f15727a44b3ada2bfa7132
baeggoose <bsj154@gmail.com>
2023-08-21 13:35:53 +0900

    타입별칭_코드 가독성 좋음

---
+++ class-note/4_type_aliases.ts
@@ -0,0 +1,20 @@
+// interface human {
+//   name: string;
+//   age: number;
+// }
+
+type human = {
+  name: string;
+  age: number;
+};
+
+var baeggoose: human = {
+  name: "구스",
+  age: 10,
+};
+
+type MyString = string;
+var STR: MyString = "hi";
+
+type Todo = { id: string; title: string; done: boolean };
+function getTodo(todo: Todo) {}
