374adf370d6e4b51674c65056c10a47af58af674
baeggoose <bsj154@gmail.com>
2023-08-21 09:53:34 +0900

    인터페이스_함수 활용

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -8,3 +8,14 @@ var baeggoose: User = {
   age: 28,
   name: "백구스",
 };
+
+// 함수에 활용한 인터페이스
+function getUser(user: User) {
+  console.log(user);
+}
+const goose = {
+  name: "구스",
+  age: 100,
+};
+
+getUser(goose);
