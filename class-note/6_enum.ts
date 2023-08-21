c0bb9b7ade034200807ad8ca4f5ddf66514314fc
baeggoose <bsj154@gmail.com>
2023-08-21 19:52:12 +0900

    Enum_특정 값들의 집합,문자형 이넘과 숫자형 이넘을 지원

---
+++ class-note/6_enum.ts
@@ -0,0 +1,23 @@
+enum Shoes {
+  Nike = "나이키",
+  Adidas = "아디다스",
+}
+var myShoes = Shoes.Nike;
+console.log(myShoes); //나이키
+
+// 예제
+enum Answer {
+  Yes = "Y",
+  No = "N",
+}
+
+function askQuestion(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log("정답입니다");
+  }
+  if (answer === Answer.No) {
+    console.log("오답입니다");
+  }
+}
+// 이넘에서 제공되는 값만 넘길 수 있다
+askQuestion(Answer.Yes);
