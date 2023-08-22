148bf300485456005f3dda7d4e213f4059ff6a81
baeggoose <bsj154@gmail.com>
2023-08-22 12:49:15 +0900

    TS class 문법

---
+++ class-note/7_class.ts
@@ -0,0 +1,12 @@
+class Prsn {
+  // ptivate -> 클래스 내에서만 유효
+  private name: string;
+  public age: number;
+  // readonly -> 읽기 가능, 수정 불가
+  readonly log: string;
+
+  constructor(name: string, age: number) {
+    this.name;
+    this.age;
+  }
+}
