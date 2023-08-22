b7fbb1d26040319e7c98aab99e44dcd3be4c24ab
baeggoose <bsj154@gmail.com>
2023-08-22 16:10:15 +0900

    interface generics example

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,21 @@
-const emails: { value: string; selected: boolean }[] = [
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+
+const emails: DropdownItem<string>[] = [
   { value: "naver.com", selected: true },
   { value: "gmail.com", selected: false },
   { value: "hanmail.net", selected: false },
 ];
 
-const numberOfProducts: { value: number; selected: boolean }[] = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: { value: string; selected: boolean }) {
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
   const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
