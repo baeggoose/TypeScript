f30164d53f4fc97c25d385967358578e164c4f87
baeggoose <bsj154@gmail.com>
2023-08-23 19:48:27 +0900

    type interence 인터페이스와 제네릭을 이용한 타입 추론

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -1,6 +1,17 @@
-var a;
+// 타입 추론 기본 1
+var a = "ㅁ";
 
-function getB(b = 10) {
+function get1B(b = 10) {
   var c = "hi";
   return b + c;
 }
+
+// 타입 추론 기본 1
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+var shoppingItem: Dropdown<string> = {
+  value: "abs",
+  title: "shop",
+};
